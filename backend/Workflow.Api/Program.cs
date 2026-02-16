de5f98172c4fe4cbc607d63a08c06bced018d015
Raphael Neves <coder@DESKTOP-1DI7H1C.localdomain>
2026-02-16 02:59:59 -0300

    connection win-wsl working

--- backend/Workflow.Api/Program.cs
+++ backend/Workflow.Api/Program.cs
@@ -1,3 +1,6 @@
+using Dapper;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,29 +19,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/db-check", async (IConfiguration config) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    using var connection = new NpgsqlConnection(config.GetConnectionString("DefaultConnection"));
+    var time = await connection.QueryFirstAsync<DateTime>("SELECT NOW()");
+    return Results.Ok(new { Status = "Connected", DatabaseTime = time });
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
